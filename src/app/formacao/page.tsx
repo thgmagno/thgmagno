1342dbe5b81bfde40643720277f44a36b0f1cc4b
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-06 16:12:56 -0300

    update

--- src/app/formacao/page.tsx
+++ src/app/formacao/page.tsx
@@ -1,9 +1,10 @@
+import { ToggleCategory } from '@/components/projects/ToggleCategory'
 import { Wrapper } from '@/components/wrapper'
 
 export default function Formacao() {
   return (
     <Wrapper title="Formação">
-      <h1>page Component</h1>
+      <ToggleCategory />
     </Wrapper>
   )
 }
