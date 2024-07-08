0589b0ec6fe1169439ae6cabcef641fb1d9d5a27
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-07 21:19:11 -0300

    update

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,11 +1,37 @@
 import { GridProjects } from '@/components/projects/GridProjects'
 import { Wrapper } from '@/components/wrapper'
+import { FeaturesType, ProjectType, TechnologiesType } from '@/lib/types'
 
 export default function Projetos() {
+  const data: ProjectType = {
+    title: 'Aluno Connect',
+    slug: 'aluno-connect',
+    description:
+      'Ferramenta de gestão de frequências voltado para o ambiente acadêmico estudantil - foi uma iniciativa de integração ao curso Técnico em Informática ofertado pelo Serviço Nacional de Aprendizagem Industrial - Senai, como parte dos requisitos para obtenção do título de Técnico de Informática.',
+    projectImgUrl:
+      'https://cdn.cosmicjs.com/971c28a0-3350-11ef-bfb7-a598d323cbfc-student.webp',
+    projectUrl: 'https://aluno-connect.vercel.app/',
+    objective:
+      'O projeto propõe uma forma inovadora de gerenciar a frequência dentro de uma instituição de ensino. Com a premissa de proporcionar uma experiência avançada e eficaz, a plataforma visa ir além do simples controle de presença, incorporando funcionalidade para otimizar a administração acadêmica, desde o acompanhamento em tempo real e até notificações automáticas via e-mail. O Aluno Connect se destaca como uma solução integral e inteligente para facilitar a jornada acadêmica, proporcionando uma gestão de frequência eficiente e contribuindo para o engajamento dos estudantes. Nesse contexto, a gestão de frequência fornecida pelo Aluno Connect torna-se uma ferramenta essencial para aprimorar o processo educacional. Ao incentivar uma participação ativa e construtiva dos estudantes, a plataforma se torna um elemento-chave para melhorar a qualidade de ensino.',
+  }
+
+  // const features: FeaturesType = {}
+
+  const technologies: TechnologiesType = {
+    slug: 'aluno-connect',
+    metadata: {
+      title: 'Next',
+      url: 'https://nextjs.org/',
+    },
+  }
+
+  const dataMock = Array.from({ length: 10 }, () => ({ ...data }))
+
+  const techMock = Array.from({ length: 10 }, () => ({ ...technologies }))
+
   return (
     <Wrapper title="Projetos">
-      <h1>page Component</h1>
-      <GridProjects />
+      <GridProjects projects={dataMock} technologies={techMock} />
     </Wrapper>
   )
 }
--- src/components/projects/GridProjects.tsx
+++ src/components/projects/GridProjects.tsx
@@ -1,7 +1,51 @@
-export function GridProjects() {
+import { FeaturesType, ProjectType, TechnologiesType } from '@/lib/types'
+import { ArrowUpRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Badge } from '../ui/badge'
+
+interface Props {
+  projects: ProjectType[]
+  // features: FeaturesType[]
+  technologies: TechnologiesType[]
+}
+
+export function GridProjects({ projects, technologies }: Props) {
   return (
-    <div>
-      <h1>GridProjects Component</h1>
-    </div>
+    <section className="mt-5 grid gap-6 md:grid-cols-2">
+      {projects.map((item) => (
+        <article
+          key={item.slug}
+          className="relative cursor-default overflow-hidden rounded-lg border-l-4 border-slate-600 bg-neutral-800 py-3 pl-2 text-white"
+        >
+          <div className="relative z-10 max-h-20 max-w-[70%]">
+            <b>{item.title}</b>
+            <p className="text-sm font-light">
+              {item.description.slice(0, 80).concat('...')}
+            </p>
+          </div>
+          <div className="relative z-10 mr-3 mt-10 flex justify-between md:mt-6">
+            <div className="no-scrollbar flex max-w-[60%] gap-1 overflow-x-scroll md:max-w-[70%]">
+              {technologies
+                .filter((tech) => tech.slug === item.slug)
+                .map((tech) => (
+                  <Badge className="bg-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-950">
+                    <Link href={tech.metadata.url}>{tech.metadata.title}</Link>
+                  </Badge>
+                ))}
+            </div>
+            <button className="flex items-center gap-1 rounded-md border px-2 py-1 text-sm hover:bg-neutral-700 active:scale-95">
+              Saiba mais
+            </button>
+          </div>
+          <Image
+            src={item.projectImgUrl}
+            fill
+            alt=""
+            className="absolute left-0 top-0 object-cover opacity-20 dark:opacity-10"
+          />
+        </article>
+      ))}
+    </section>
   )
 }
