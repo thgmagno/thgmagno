075bea6cb19dc2c453be97bdd419c64eb29b6d11
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-10 10:35:45 -0300

    update layout responsive

--- src/components/projects/GridProjects.tsx
+++ src/components/projects/GridProjects.tsx
@@ -30,12 +30,12 @@ export function GridProjects({ projects }: Props) {
         >
           <ImageProject url={item.image.url} title={item.data.title} />
           <div className="relative z-10 mb-8 mt-5 max-h-20 pl-2">
-            <div className="mb-2 flex justify-between">
+            <div className="mb-2 flex flex-wrap-reverse justify-between">
               <b>{item.data.title}</b>
               <StatusProject done={item.done} />
             </div>
-            <p className="pr-3 text-justify text-sm font-light">
-              {item.data.description[language].slice(0, 130).concat('...')}
+            <p className="truncate pr-3 text-justify text-sm font-light sm:overflow-visible sm:whitespace-normal">
+              {item.data.description[language].slice(0, 120).concat('...')}
             </p>
           </div>
           <div className="relative z-10 mx-2 flex justify-between md:mt-6">
@@ -43,14 +43,17 @@ export function GridProjects({ projects }: Props) {
               {item.data.technologies
                 .sort((a, b) => a.title.localeCompare(b.title))
                 .map((tech) => (
-                  <Badge className="bg-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-950">
+                  <Badge
+                    key={tech.title}
+                    className="min-w-fit bg-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-950"
+                  >
                     <Link href={tech.url}>{tech.title}</Link>
                   </Badge>
                 ))}
             </div>
             <Link
               href={`/projetos/${item.data.slug}`}
-              className="flex items-center gap-1 rounded-md border bg-neutral-600 px-2 py-1 text-sm text-neutral-100 hover:bg-neutral-700 active:scale-95 dark:hover:bg-neutral-700"
+              className="flex min-w-fit items-center gap-1 rounded-md border bg-neutral-600 px-2 py-1 text-sm text-neutral-100 hover:bg-neutral-700 active:scale-95 dark:hover:bg-neutral-700"
             >
               {dict[language].label}
             </Link>
