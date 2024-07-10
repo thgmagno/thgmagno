7c9516bd16b1cb6b65cf538d3185ca1569957daa
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-10 11:57:48 -0300

    update projects

--- src/components/projects/ViewProject.tsx
+++ src/components/projects/ViewProject.tsx
@@ -3,7 +3,6 @@
 import { CosmicResponse } from '@/lib/cosmic.types'
 import { useLanguageStore } from '@/lib/store/languageStore'
 import Image from 'next/image'
-import { ReactNode } from 'react'
 import { Badge } from '../ui/badge'
 import Link from 'next/link'
 import { StatusProject } from './StatusProject'
@@ -23,19 +22,6 @@ export function ViewProject({ project }: Props) {
     month: 'long',
     year: 'numeric',
   })
-
-  const Wrapper = ({
-    title,
-    children,
-  }: {
-    title: string
-    children: ReactNode
-  }) => (
-    <section>
-      <label className="underline">{title}</label>
-      {children}
-    </section>
-  )
 
   const dicts = {
     publishedAt: {
@@ -72,44 +58,51 @@ export function ViewProject({ project }: Props) {
         <StatusProject done={project.done as boolean} />
       </section>
 
-      <Wrapper title={dicts.description[language]}>
-        <p className="font-light">
-          {project.data?.description[language] as string}
-        </p>
-      </Wrapper>
+      <p className="mt-3 font-light">
+        {project.data?.description[language] as string}
+      </p>
 
       <div className="relative mx-auto my-8 h-[400px] w-full opacity-90 sm:max-w-[90%] md:max-w-[60%]">
         <Image
           src={project.image?.url as string}
-          alt={''}
+          alt={`Image of ${project.data?.title}`}
           layout="fill"
           objectFit="cover"
           className="w-full rounded-lg object-cover shadow ring-2 ring-neutral-500 dark:ring-zinc-600"
         />
       </div>
 
-      <Wrapper title={dicts.objective[language]}>
-        <p className="font-light">
-          {project.data?.objective[language] as string}
-        </p>
-      </Wrapper>
-
-      <Wrapper title={dicts.features[language]}>
-        {project.data?.features[language].map((item) => (
-          <p key={item.title}>
-            <b>{item.title}:</b>{' '}
-            <span className="font-light">{item.description}</span>
+      <div className="flex flex-col space-y-5">
+        <div className="flex flex-col">
+          <h2 className="font-medium">{dicts.objective[language]}:</h2>
+          <p className="font-light">
+            {project.data?.objective[language] as string}
           </p>
-        ))}
-      </Wrapper>
+        </div>
 
-      <Wrapper title={dicts.technologies[language]}>
-        {project.data?.technologies.map((item) => (
-          <Link href={item.url} key={item.title}>
-            <Badge>{item.title}</Badge>
-          </Link>
-        ))}
-      </Wrapper>
+        <div className="flex flex-col">
+          <h2 className="font-medium">{dicts.features[language]}:</h2>
+          <ul className="list-inside list-disc">
+            {project.data?.features[language].map((item) => (
+              <li key={item.title}>
+                <b>{item.title}:</b>{' '}
+                <span className="font-light">{item.description}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex flex-col">
+          <h2 className="font-medium">{dicts.technologies[language]}:</h2>
+          <div className="flex flex-wrap gap-1">
+            {project.data?.technologies.map((item) => (
+              <Link href={item.url} key={item.title}>
+                <Badge>{item.title}</Badge>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
