a5089b248574ae249ae8bbd8847398d19fd22905
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-22 10:04:04 -0300

    update projects

--- src/components/projects/GridProjects.tsx
+++ src/components/projects/GridProjects.tsx
@@ -23,43 +23,45 @@ export function GridProjects({ projects }: Props) {
 
   return (
     <section className="mt-5 grid gap-6 md:grid-cols-2">
-      {projects.map((item) => (
-        <article
-          key={item.data.title}
-          className="cursor-default overflow-hidden rounded-lg border-l-4 border-slate-600 bg-neutral-100 pb-3 shadow-md dark:bg-neutral-800"
-        >
-          <ImageProject url={item.image.url} title={item.data.title} />
-          <div className="relative z-10 mb-8 mt-5 max-h-20 pl-2">
-            <div className="mb-2 flex flex-wrap-reverse justify-between">
-              <b>{item.data.title}</b>
-              <StatusProject done={item.done} />
+      {projects
+        .sort((a, b) => a.data.title.localeCompare(b.data.title))
+        .map((item) => (
+          <article
+            key={item.data.title}
+            className="cursor-default overflow-hidden rounded-lg border-l-4 border-slate-600 bg-neutral-100 pb-3 shadow-md dark:bg-neutral-800"
+          >
+            <ImageProject url={item.image.url} title={item.data.title} />
+            <div className="relative z-10 mb-8 mt-5 max-h-20 pl-2">
+              <div className="mb-2 flex flex-wrap-reverse justify-between">
+                <b>{item.data.title}</b>
+                <StatusProject done={item.done} />
+              </div>
+              <p className="truncate pr-3 text-justify text-sm font-light sm:overflow-visible sm:whitespace-normal">
+                {item.data.description[language].slice(0, 120).concat('...')}
+              </p>
             </div>
-            <p className="truncate pr-3 text-justify text-sm font-light sm:overflow-visible sm:whitespace-normal">
-              {item.data.description[language].slice(0, 120).concat('...')}
-            </p>
-          </div>
-          <div className="relative z-10 mx-2 flex justify-between md:mt-6">
-            <div className="no-scrollbar flex max-w-[60%] gap-1 overflow-x-scroll md:max-w-[70%]">
-              {item.data.technologies
-                .sort((a, b) => a.title.localeCompare(b.title))
-                .map((tech) => (
-                  <Badge
-                    key={tech.title}
-                    className="min-w-fit bg-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-950"
-                  >
-                    <Link href={tech.url}>{tech.title}</Link>
-                  </Badge>
-                ))}
+            <div className="relative z-10 mx-2 flex justify-between md:mt-6">
+              <div className="no-scrollbar flex max-w-[60%] gap-1 overflow-x-scroll md:max-w-[70%]">
+                {item.data.technologies
+                  .sort((a, b) => a.title.localeCompare(b.title))
+                  .map((tech) => (
+                    <Badge
+                      key={tech.title}
+                      className="min-w-fit bg-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-950"
+                    >
+                      <Link href={tech.url}>{tech.title}</Link>
+                    </Badge>
+                  ))}
+              </div>
+              <Link
+                href={`/projetos/${item.data.slug}`}
+                className="flex min-w-fit items-center gap-1 rounded-md border bg-neutral-600 px-2 py-1 text-sm text-neutral-100 hover:bg-neutral-700 active:scale-95 dark:hover:bg-neutral-700"
+              >
+                {dict[language].label}
+              </Link>
             </div>
-            <Link
-              href={`/projetos/${item.data.slug}`}
-              className="flex min-w-fit items-center gap-1 rounded-md border bg-neutral-600 px-2 py-1 text-sm text-neutral-100 hover:bg-neutral-700 active:scale-95 dark:hover:bg-neutral-700"
-            >
-              {dict[language].label}
-            </Link>
-          </div>
-        </article>
-      ))}
+          </article>
+        ))}
     </section>
   )
 }
