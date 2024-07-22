b6175473c7da2e4fd8ecea1c9797bf47f20faa96
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-07-22 10:04:04 -0300

    update projects

--- src/components/projects/ViewProject.tsx
+++ src/components/projects/ViewProject.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import { Badge } from '../ui/badge'
 import Link from 'next/link'
 import { StatusProject } from './StatusProject'
+import { ArrowUpRight } from 'lucide-react'
 
 type Props = {
   project: Partial<CosmicResponse['object']['metadata']['projects'][0]>
@@ -44,6 +45,10 @@ export function ViewProject({ project }: Props) {
       portuguese: 'Tecnologias',
       english: 'Technologies',
     },
+    website: {
+      portuguese: 'Visitar o site',
+      english: 'Visit the website',
+    },
   }
 
   return (
@@ -70,19 +75,29 @@ export function ViewProject({ project }: Props) {
           objectFit="cover"
           className="w-full rounded-lg object-cover shadow ring-2 ring-neutral-500 dark:ring-zinc-600"
         />
+        <div className="absolute -bottom-8 left-0 right-0 flex items-center justify-center">
+          <div className="mr-3 flex-1 border-b border-neutral-500" />
+          <Link
+            target="_blank"
+            href={project.data?.url as string}
+            className="flex items-center justify-end gap-2.5 text-sm hover:underline active:scale-95"
+          >
+            {dicts.website[language]} <ArrowUpRight size={16} />
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-col space-y-5">
-        <div className="flex flex-col">
+      <div className="mt-5 flex flex-col space-y-5">
+        <div className="flex flex-col space-y-3">
           <h2 className="font-medium">{dicts.objective[language]}:</h2>
           <p className="font-light">
             {project.data?.objective[language] as string}
           </p>
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-3">
           <h2 className="font-medium">{dicts.features[language]}:</h2>
-          <ul className="list-inside list-disc">
+          <ul className="list-inside list-disc space-y-3">
             {project.data?.features[language].map((item) => (
               <li key={item.title}>
                 <b>{item.title}:</b>{' '}
@@ -92,14 +107,16 @@ export function ViewProject({ project }: Props) {
           </ul>
         </div>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-3">
           <h2 className="font-medium">{dicts.technologies[language]}:</h2>
           <div className="flex flex-wrap gap-1">
-            {project.data?.technologies.map((item) => (
-              <Link href={item.url} key={item.title}>
-                <Badge>{item.title}</Badge>
-              </Link>
-            ))}
+            {project.data?.technologies
+              .sort((a, b) => a.title.localeCompare(b.title))
+              .map((item) => (
+                <Link href={item.url} key={item.title}>
+                  <Badge>{item.title}</Badge>
+                </Link>
+              ))}
           </div>
         </div>
       </div>
