c8e8eb24abd8e30b76933e88da23662f7084f5c4
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-08-12 19:59:01 -0300

    add link to curriculum

--- src/app/formacao/page.tsx
+++ src/app/formacao/page.tsx
@@ -2,6 +2,8 @@ import { fetchData } from '@/actions'
 import { EducationFilters } from '@/components/education/EducationFilters'
 import { EducationGrid } from '@/components/education/EducationGrid'
 import { Wrapper } from '@/components/wrapper'
+import { ArrowUpRight } from 'lucide-react'
+import Link from 'next/link'
 
 export default async function Formacao({
   searchParams,
@@ -19,7 +21,16 @@ export default async function Formacao({
 
   return (
     <Wrapper>
-      <EducationFilters education={education} />
+      <div className="flex items-center justify-between">
+        <EducationFilters education={education} />
+        <Link
+          target="_blank"
+          href="https://me-green-tau.vercel.app/"
+          className="flex items-center gap-1 border-neutral-600 text-sm hover:border-b"
+        >
+          Acessar CV <ArrowUpRight size={20} />
+        </Link>
+      </div>
       <EducationGrid education={dataSorted} />
     </Wrapper>
   )
