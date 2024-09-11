96c36f4b4680a08255b0a1a08e2ebc1d8d1941e6
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-09-11 19:45:02 -0300

    latest version restored

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+import Link from 'next/link'
+
+export default function AdminPage() {
+  return <Link href="/admin/adicionar-projeto">Adicionar projeto</Link>
+}
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -31,3 +31,45 @@ export type TechnologiesType = {
     url: string
   }
 }
+
+export interface MultiLangContent {
+  portuguese: string
+  english: string
+}
+
+export interface Feature {
+  title: string
+  description: string
+}
+
+export interface Technology {
+  title: string
+  url: string
+}
+
+export interface ImageUrl {
+  small: string
+  medium: string
+}
+
+export interface FormData {
+  featured: boolean
+  title: MultiLangContent
+  slug: string
+  description: MultiLangContent
+  objective: MultiLangContent
+  deployUrl: string
+  videoUrl: MultiLangContent
+  features: {
+    portuguese: Feature[]
+    english: Feature[]
+  }
+  technologies: {
+    portuguese: Technology[]
+    english: Technology[]
+  }
+  imageUrl: ImageUrl
+  createdAt: string
+  done: boolean
+  token: string
+}
