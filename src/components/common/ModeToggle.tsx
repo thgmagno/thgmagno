6c02ebbfbbb72f7d6f7f7610bd30f0f5497f8f1c
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-10-08 11:22:10 -0300

    update ModeToggle.tsx

--- src/components/common/ModeToggle.tsx
+++ src/components/common/ModeToggle.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import * as React from 'react'
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import clsx from 'clsx'
+import { useEffect } from 'react'
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
+
+  useEffect(() => {
+    const storagedTheme = localStorage.getItem('theme')
+    if (storagedTheme && storagedTheme !== theme) {
+      localStorage.setItem('theme', theme || 'dark')
+    }
+  }, [theme])
 
   return (
     <button>
