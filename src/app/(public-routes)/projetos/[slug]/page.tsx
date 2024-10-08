9afd84ec1083d5c87a4918a38a01fa054928062e
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-10-08 19:45:10 -0300

    add link to website

--- src/app/(public-routes)/projetos/[slug]/page.tsx
+++ src/app/(public-routes)/projetos/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import { findOneProject } from '@/server/actions'
+import { ArrowUpRight } from 'lucide-react'
+import Link from 'next/link'
 import { Suspense } from 'react'
 
 export default async function SlugProjeto({
@@ -19,17 +21,31 @@ export default async function SlugProjeto({
         <p className="prose prose-neutral dark:prose-invert pt-3">
           {project.description}
         </p>
-        {youtubeVideoId !== '' && (
-          <div className="pt-6">
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${youtubeVideoId}`}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
+        {youtubeVideoId && (
+          <div className="mx-auto w-[95%] max-w-2xl pt-6">
+            <div
+              className="relative h-0 w-full"
+              style={{ paddingTop: '56.25%' }}
+            >
+              <iframe
+                className="absolute left-0 top-0 h-full w-full rounded-xl"
+                src={`https://www.youtube.com/embed/${youtubeVideoId}`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
           </div>
+        )}
+        {project.website_url && (
+          <Link
+            target="_blank"
+            href={project.website_url}
+            className="my-5 flex items-center justify-center hover:underline"
+          >
+            Conhecer o site
+            <ArrowUpRight className="ml-2 h-5 w-5" />
+          </Link>
         )}
       </section>
     </Suspense>
