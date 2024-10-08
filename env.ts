b60abe3eeff8c7e69623a89aff1f864ec8412ba2
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-10-07 23:47:23 -0300

    update baseUrl

--- env.ts
+++ env.ts
@@ -6,6 +6,7 @@ const schema = z.object({
   ADMIN_PASSWORD: z.string(),
   AUTH_SECRET: z.string(),
   COOKIE_NAME: z.string(),
+  BASE_URL: z.string().url(),
 })
 
 export const env = schema.parse(process.env)
