e299516feb5461bfd215191b75a55720c209106d
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-10-08 19:24:58 -0300

    add embed video youtube url

--- src/app/(public-routes)/projetos/[slug]/page.tsx
+++ src/app/(public-routes)/projetos/[slug]/page.tsx
@@ -7,6 +7,8 @@ export default async function SlugProjeto({
   params: { slug: string }
 }) {
   const project = await findOneProject(params.slug)
+  const youtubeVideoUrl = project.presentation_video_url || ''
+  const youtubeVideoId = youtubeVideoUrl.split('/').pop() || ''
 
   return (
     <Suspense fallback={'Carregando...'}>
@@ -17,7 +19,21 @@ export default async function SlugProjeto({
         <p className="prose prose-neutral dark:prose-invert pt-3">
           {project.description}
         </p>
+        {youtubeVideoId !== '' && (
+          <div className="pt-6">
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${youtubeVideoId}`}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        )}
       </section>
     </Suspense>
   )
 }
+
+// https://youtu.be/X1DSpg5_hvc
