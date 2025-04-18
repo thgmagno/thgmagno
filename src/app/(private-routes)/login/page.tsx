40403f9144973a22182f98848196b01e218d5653
Thiago Magno <thgmgn@gmail.com>
2025-04-18 16:44:19 -0300

    update

--- src/app/(private-routes)/login/page.tsx
+++ src/app/(private-routes)/login/page.tsx
@@ -1,20 +1,33 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { Github } from 'lucide-react'
+import { Github, HomeIcon } from 'lucide-react'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 export default function LoginPage() {
   const handleSignIn = async () => await signIn('github')
+  const { replace } = useRouter()
 
   return (
-    <section className="flex h-screen w-full items-center justify-center">
+    <section className="flex h-[50vh] w-full flex-col items-center justify-center gap-4">
+      <span className="text-sm font-light text-amber-400">
+        Somente usuários autorizados
+      </span>
       <Button
         onClick={handleSignIn}
-        className="flex items-center justify-center gap-2.5 p-6 text-lg"
+        className="flex w-full items-center justify-center gap-2.5 p-6 text-lg"
       >
         <Github />
         Entrar com GitHub
+      </Button>
+      <Button
+        onClick={() => replace('/')}
+        className="flex w-full items-center justify-center gap-2.5 p-6 text-lg"
+        variant="outline"
+      >
+        <HomeIcon />
+        Página inicial
       </Button>
     </section>
   )
