1fd673f7822dc45d7472c54fda38b4d6fdf47971
Thiago Magno <thgmgn@gmail.com>
2025-04-18 17:30:02 -0300

    update

--- src/components/admin/Comments.tsx
+++ src/components/admin/Comments.tsx
@@ -11,54 +11,57 @@ export async function Comments() {
   const comments = await actions.social.findAllComments()
 
   return (
-    <CustomCard>
-      <div className="flex items-center justify-between">
+    <>
+      <div className="mb-4 flex items-center justify-between">
         <h2 className="font-semibold sm:text-xl">Comentários dos projetos</h2>
       </div>
-      <ScrollArea className="h-[30rem]">
-        {comments.map((comment) => (
-          <div key={comment.id} className="relative space-y-3 border-b p-4">
-            <div className="flex justify-between">
-              <div className="flex space-x-2">
-                <Avatar className="h-9 w-9">
-                  <AvatarImage src={comment.avatarUrl || ''} />
-                  <AvatarFallback>
-                    {comment.authorName?.slice(0, 2).toUpperCase() ||
-                      comment.authorEmail?.slice(0, 2).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-                <div>
-                  <Link
-                    href={`/projetos/${comment.projectName}`}
-                    className="group block text-sm text-blue-500 transition-opacity duration-200 hover:opacity-80"
-                  >
-                    {comment.projectName}
-                  </Link>
-                  <p className="flex flex-col-reverse text-sm sm:flex-row sm:items-center">
-                    <span>{comment.authorName}</span>
-                    <span className="min-w-fit text-xs text-neutral-400 sm:ml-2">
-                      {formatDistanceToNow(new Date(comment.createdAt), {
-                        addSuffix: true,
-                        locale: ptBR,
-                      })}
-                    </span>
-                  </p>
-                  <p className="text-muted-foreground text-xs font-light">
-                    {comment.authorEmail}
-                  </p>
+      <ScrollArea className="h-[36rem] pr-4">
+        <div className="grid gap-4">
+          {comments.map((comment) => (
+            <CustomCard key={comment.id}>
+              <div className="relative space-y-3 p-1">
+                <div className="flex justify-between">
+                  <div className="flex space-x-2">
+                    <Avatar className="h-9 w-9">
+                      <AvatarImage src={comment.avatarUrl || ''} />
+                      <AvatarFallback>
+                        {comment.authorName?.slice(0, 2).toUpperCase() ||
+                          comment.authorEmail?.slice(0, 2).toUpperCase()}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <Link
+                        href={`/projetos/${comment.projectName}`}
+                        className="group block text-sm text-blue-500 transition-opacity duration-200 hover:opacity-80"
+                      >
+                        {comment.projectName}
+                      </Link>
+                      <p className="flex flex-col-reverse text-sm sm:flex-row sm:items-center">
+                        <span>{comment.authorName}</span>
+                        <span className="min-w-fit text-xs text-neutral-400 sm:ml-2">
+                          {formatDistanceToNow(new Date(comment.createdAt), {
+                            addSuffix: true,
+                            locale: ptBR,
+                          })}
+                        </span>
+                      </p>
+                      <p className="text-muted-foreground text-xs font-light">
+                        {comment.authorEmail}
+                      </p>
+                    </div>
+                  </div>
+                  <DeleteCommentButton
+                    comment={comment.comment}
+                    commentId={comment.id}
+                    projectId={comment.projectId}
+                  />
                 </div>
+                <div className="text-sm">{comment.comment}</div>
               </div>
-              <DeleteCommentButton
-                comment={comment.comment}
-                commentId={comment.id}
-                projectId={comment.projectId}
-              />
-            </div>
-
-            <div className="text-sm">{comment.comment}</div>
-          </div>
-        ))}
+            </CustomCard>
+          ))}
+        </div>
       </ScrollArea>
-    </CustomCard>
+    </>
   )
 }
