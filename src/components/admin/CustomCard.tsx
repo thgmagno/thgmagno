e93d625fa55de53df269ac46e021b1a0cf445561
Thiago Magno <thgmgn@gmail.com>
2025-04-19 16:02:08 -0300

    update

--- src/components/admin/CustomCard.tsx
+++ src/components/admin/CustomCard.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export function CustomCard({ children }: { children: ReactNode }) {
   return (
-    <div className="bg-card flex flex-col gap-2 rounded-2xl border p-4 shadow-sm">
+    <div className="bg-card flex max-h-72 w-full flex-col gap-2 overflow-auto rounded-2xl border p-4 shadow-sm">
       {children}
     </div>
   )
